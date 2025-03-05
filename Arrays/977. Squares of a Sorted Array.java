cb9aa024a2fb750fec1a8355be0491636f2f3de1
Jainam Savla <jainam.18201@sakec.ac.in>
2025-03-06 03:50:22 +0530

    Create 977. Squares of a Sorted Array.java

---
+++ Arrays/977. Squares of a Sorted Array.java
@@ -0,0 +1,12 @@
+//977. Squares of a Sorted Array
+//7 ms beats 44.06% coders
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int [] temp=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            temp[i]=nums[i]*nums[i];
+        }
+       Arrays.sort(temp);
+        return temp;
+    }
+}
