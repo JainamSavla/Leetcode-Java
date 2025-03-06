6d0c595b8cd6797fb08c9203442d3a4a2d7f45b9
Jainam Savla <jainam.18201@sakec.ac.in>
2025-03-07 02:31:22 +0530

    Update 0977. Squares of a Sorted Array.java

--- Arrays/0977. Squares of a Sorted Array.java
+++ Arrays/0977. Squares of a Sorted Array.java
@@ -10,3 +10,26 @@ class Solution {
         return temp;
     }
 }
+//TWO POINTER 
+//1ms beets 100% 
+class Solution {
+    public int[] sortedSquares(int[] nums) {  
+        int n = nums.length;
+        int l = 0;
+        int r = nums.length - 1;
+        int[] result = new int[n];
+
+        for (int i = r; i >= 0; i--) {
+            int val; 
+            if (Math.abs(nums[l]) > Math.abs(nums[r])) {
+              result[i] = nums[l] * nums[l];
+              l++;
+            } else {
+            result[i] = nums[r] * nums[r];
+             r--;
+            }
+        }
+         return result;
+    }
+}
+
