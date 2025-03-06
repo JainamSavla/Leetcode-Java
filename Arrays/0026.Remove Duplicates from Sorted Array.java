1f4018cd01501a26801fde06bd32719333328adc
Jainam Savla <jainam.18201@sakec.ac.in>
2025-03-07 02:33:32 +0530

    Create 0026.Remove Duplicates from Sorted Array.java

---
+++ Arrays/0026.Remove Duplicates from Sorted Array.java
@@ -0,0 +1,17 @@
+//Remove Duplicates from Sorted Array
+class Solution {
+    public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0; // Handle empty case
+
+    int k = 1; // Start from index 1 (nums[0] is always unique)
+
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) { 
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+  }
+}
