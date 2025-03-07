3363d9d32711e27af1ec898b0f36a05450d17de6
Jainam Savla <jainam.18201@sakec.ac.in>
2025-03-08 02:22:16 +0530

    Update 2460. Apply Operations to an Array.java

--- Arrays/2460. Apply Operations to an Array.java
+++ Arrays/2460. Apply Operations to an Array.java
@@ -20,3 +20,32 @@ class Solution {
         return result;
     }
 }
+
+//Approach-2
+//IMPROVED SPACE COMPLEXITY O(1) TO 43.50MB Beats 87.87% 
+
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
+                k++;
+            }
+        }
+        
+        return nums;
+    }
+}
+
