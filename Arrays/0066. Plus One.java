afdd0db4dc7e1ecd0ab4f5ef84cbadd13b6f7007
Jainam Savla <jainam.18201@sakec.ac.in>
2025-03-13 22:08:43 +0530

    Update 0066. Plus One.java

--- Arrays/0066. Plus One.java
+++ Arrays/0066. Plus One.java
@@ -1,19 +1,22 @@
-//0066. Plus One
-//Runtime 0 ms Beats 100.00%  O(n)
-//Memory 42.07 MB Beats 30.10% O(n)
 class Solution {
     public int[] plusOne(int[] digits) {
+        // Iterate through the array from the last digit to the first
         for (int i = digits.length - 1; i >= 0; i--) {
+            
+            // If the current digit is less than 9, simply increment it by 1
             if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
+                digits[i]++;  // Increment the digit
+                return digits;  // Return the array since no further changes are needed
             }
+            
+            // If the current digit is 9, set it to 0 and continue the loop to handle carry-over
             digits[i] = 0;
         }
         
-        // If all digits were 9, we need a bigger array
+        // If all digits were 9, the loop completes and all digits are now 0
+        // In this case, we need a new array with an extra digit to accommodate the carry
         int[] newArray = new int[digits.length + 1];
-        newArray[0] = 1;
-        return newArray;
+        newArray[0] = 1;  // Set the first digit to 1, the rest are already 0 by default
+        return newArray;  // Return the new array
     }
 }
