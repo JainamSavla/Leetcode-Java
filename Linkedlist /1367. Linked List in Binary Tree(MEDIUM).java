6eeebc2d7f1b0fb88955cfd7b5666c608a7b6cf6
Jainam Savla <jainam.18201@sakec.ac.in>
2025-03-24 21:36:07 +0530

    Create 1367. Linked List in Binary Tree(MEDIUM).java

---
+++ Linkedlist /1367. Linked List in Binary Tree(MEDIUM).java
@@ -0,0 +1,51 @@
+//1 ms Beats 99.18% TIME COMPLEXITY:O(N∗M)
+//N = Number of nodes in the binary tree. M = Length of the linked list.
+//44.74 MB Beats 93.98%SPACE COMPLEXITY:O(M+N).
+//dfs() takes O(M) stack space.isSubPath() takes O(N) stack space.
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean dfs(ListNode head, TreeNode root) {
+        if (head == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        if (root.val != head.val) {
+            return false;
+        }
+
+        return dfs(head.next, root.left) || dfs(head.next, root.right);
+    }
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) { // If tree is empty, return false
+            return false;
+        }
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+}
