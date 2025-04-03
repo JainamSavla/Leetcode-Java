b8089bb9c12eac5dc4e9d6840b4a90f9016d33e2
Jainam Savla <jainam.18201@sakec.ac.in>
2025-04-03 20:52:09 +0530

    Create 2873. Maximum Value of an Ordered Triplet I.java

---
+++ April/2873. Maximum Value of an Ordered Triplet I.java
@@ -0,0 +1,18 @@
+//Time Complexity:O(N^3)
+//Space Complexity:O(1)
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long result = 0; 
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    long value =(long)(nums[i] - nums[j]) * nums[k];
+                    result = Math.max(result, value);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
