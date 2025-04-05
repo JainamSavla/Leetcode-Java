33eae33952dfb35873086ea9526f92197969e618
Jainam Savla <jainam.18201@sakec.ac.in>
2025-04-06 02:53:18 +0530

    Update 0004. Median of Two Sorted Arrays(HARD).java

--- Arrays/0004. Median of Two Sorted Arrays(HARD).java
+++ Arrays/0004. Median of Two Sorted Arrays(HARD).java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] merged = new int[nums1.length + nums2.length];
-        System.arraycopy(nums1, 0, merged, 0, nums1.length);
+        System.arraycopy(nums1, 0, merged, 0, nums1.length);//System.arraycopy(sourceArray, srcPos, destinationArray, destPos, length);
+
         System.arraycopy(nums2, 0, merged, nums1.length, nums2.length);
         
         Arrays.sort(merged); 
