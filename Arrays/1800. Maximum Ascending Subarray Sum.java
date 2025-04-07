0356171f180d5f002f3e983dac01726b8458dffe
Jainam Savla <jainam.18201@sakec.ac.in>
2025-04-08 01:46:01 +0530

    Create 1800. Maximum Ascending Subarray Sum.java

---
+++ Arrays/1800. Maximum Ascending Subarray Sum.java
@@ -0,0 +1,18 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max=nums[0];
+        int currentsum=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                currentsum+=nums[i];
+            }else{
+                currentsum=nums[i];
+            }
+            max=Math.max(currentsum,max);
+        }
+        return max;
+    }
+}
