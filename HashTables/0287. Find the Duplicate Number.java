da20288dacf11e804619b7c048e077dc2ca75ea2
Jainam Savla <jainam.18201@sakec.ac.in>
2025-04-12 21:28:12 +0530

    Update 0287. Find the Duplicate Number.java

--- HashTables/0287. Find the Duplicate Number.java
+++ HashTables/0287. Find the Duplicate Number.java
@@ -1,14 +1,40 @@
-//Time Complexity: O(n)
-//Space Complexity:O(n)
+//TC:O(N)
+//SC:O(N)
 class Solution {
     public int findDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for(int num:nums){
-        if(set.contains(num)){
-           return num;
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num; // This is the duplicate
+            }
+            set.add(num);
         }
-        set.add(num);
+        return -1; // If no duplicate found (though problem guarantees one)
+    }
+}
+
+//APPROACH-2
+//tc:O(n)
+//sc:O(1)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int ans = 0;
+
+        for (int num : nums) {
+            int ele = Math.abs(num);
+            if (nums[ele] > 0) {
+                nums[ele] = -nums[ele];
+            } else {
+                ans = ele;
+                break;
+            }
         }
-        return -1;
+
+        // Restore the array
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
+        }
+
+        return ans;
     }
 }
