54287d38aa4e7fbf521310e260ccac463e0d8009
Jainam Savla <jainam.18201@sakec.ac.in>
2025-04-14 00:31:41 +0530

    Create 0881. Boats to Save People.java

---
+++ Arrays/0881. Boats to Save People.java
@@ -0,0 +1,21 @@
+//Time Complexity = O(n log n)
+//Space Complexity = O(1)
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int start=0;
+        int end=people.length-1;
+
+        int count=0;
+        while(start<=end){
+            if(people[start]+people[end]<=limit){
+                start++;
+                end--;
+            }else{
+                end--;
+            }
+            count++;
+        }
+        return count;
+    }
+}
